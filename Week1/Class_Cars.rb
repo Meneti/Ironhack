e9d95b3042e850cf2e60f619886fdfabffd65e79
Meneti Dissenha <menetidissenha@gmail.com>
2016-08-16 12:06:12 -0400

    2nd version

--- Week1/Class_Cars.rb
+++ Week1/Class_Cars.rb
@@ -2,6 +2,7 @@
  	def initialize (name, sound)
  		@name = name
  		@sound = sound
+ 		@cities_array = [] 
  	end
 
  	def car_noise
@@ -10,19 +11,25 @@
  	def self.star_trek_opening
 	puts " Space...the final frontier..."
 	end
-
 	def self.star_wars_openinng
 	puts "A long tome ago in a far..."
 	end
+	def travel_to (cities_name)
+	 @cities_array.push (cities_name)
+	 puts "The #{@name} visited :#{@cities_array}"
+	end
+
  end
 
  new_car = Car.new("Ferrari", "broom")
  new2_car = Car.new("Porsche", "Broooom")
+
  
 new_car.car_noise
 new2_car.car_noise
+new_car.travel_to("Miami, Boston")
+new2_car.travel_to("NY, Dallas")
 
 # Class method
-
 
 Car.star_wars_openinng
