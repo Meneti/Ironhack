62b278c7de8f9b42f3884bb9dc060dd07470d719
Meneti Dissenha <menetidissenha@gmail.com>
2016-08-18 09:27:48 -0400

    2nd commit

---
+++ Week1/Exercise_Problemsolvingdatasructures.rb
@@ -0,0 +1,11 @@
+# Part 1
+
+hash = {:wat => [0,1,{:wut => [0,[[0,1,2,3,4,5,6,7,8,{:bbq => "party"}]]]}]}
+
+p hash [:wat][2][:wut][1][0][9][:bbq]
+
+# Part 2
+
+arr = [[0,1,2,3,4,{:secret => {:unlock => [0,"BLAH"] }}]]
+
+p arr [0],[5], [:secret] => [:unlock] [1]
