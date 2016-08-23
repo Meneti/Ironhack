69a94b2b135e6e651411b33053a9a04038a91414
Meneti Dissenha <menetidissenha@gmail.com>
2016-08-23 14:40:38 -0400

    add users to Sinatra_HTTP

--- Week2/Sinatra_HTTP/app.rb
+++ Week2/Sinatra_HTTP/app.rb
@@ -21,3 +21,21 @@ get "/pizza" do
 	]
 	erb :favorite_pizza
 end
+
+users =[
+{ :username => "starwars", :name => "Star Wars", :bio => "The official home of Star Wars on Twitter", :avatar => "https://pbs.twimg.com/profile_images/631535425333518336/D-i_GqpT_400x400.jpg"},
+{ :username => "celinedion", :name => "Celine Dion", :bio => "Posts signed", :avatar => "https://pbs.twimg.com/profile_images/733642354565996544/JrzvO3Y7_400x400.jpg"},
+{ :username => "menetidissenha", :name => "Meneti Dissenha", :bio => "I like yoga", :avatar => "/menetidissenha_avatar"}
+]
+
+get "/users/:username" do 
+	@user_name_string = params[:username]
+	@the_user = users.find { |the_user| the_user[:username] == @user_name_string}
+
+	if @the_user == nil
+		erb :no_user
+	else
+		erb :user_profile
+	end
+
+end
