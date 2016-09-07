f58cb07019015377c6b81ab8bb5fe6779c893c31
Meneti Dissenha <menetidissenha@gmail.com>
2016-09-07 12:27:53 -0400

    database- sublime-timetracking

--- week4/timetracking/config/routes.rb
+++ week4/timetracking/config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
 	get '/contact', to: 'site#contact'
 
 	get'/say_name/:name', to: 'site#say_name'
+
+	get '/', to: 'site#home'
+
+	get '/projects', to: 'projects#index'
+	
 end
