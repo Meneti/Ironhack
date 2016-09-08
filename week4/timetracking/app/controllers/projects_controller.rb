d4303d9426aee765ad1a607c6feb2926ea62895b
Meneti Dissenha <menetidissenha@gmail.com>
2016-09-08 10:04:06 -0400

    add url parameters to timetracking folder

--- week4/timetracking/app/controllers/projects_controller.rb
+++ week4/timetracking/app/controllers/projects_controller.rb
@@ -3,4 +3,12 @@ class ProjectsController < ApplicationController
 		@projects = Project.order(created_at: :desc).limit(10)
 		render "index"
 	end
+
+	def show
+		@project = Project.find_by(id: params[:id])
+		unless @project 
+			render "no_projects_found"
+		end
+
+	end
 end
