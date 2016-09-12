303c785f8b8112ec221ed2a72da1339757e3e4e5
Meneti Dissenha <menetidissenha@gmail.com>
2016-09-12 18:27:58 -0400

    Music Concert interation 3

--- week4/MC/app/controllers/comments_controller.rb
+++ week4/MC/app/controllers/comments_controller.rb
@@ -8,9 +8,16 @@ class CommentsController < ApplicationController
 		@concert = Concert.find(params[:concert_id])
 		@comment = @concert.comments.new(comment_params)
     	@comment.save
+    	puts params[:comment]
     	redirect_to "/concerts/#{@concert.id}/comments"
     end
 
+    def index
+    	@concert = Concert.find(params[:concert_id])
+    	@comments = @concert.comments
+    end
+
+	private
     def comment_params
     params.require(:comment).permit(:text)
   end
--- week4/MC/app/controllers/site_controller.rb
+++ week4/MC/app/controllers/site_controller.rb
@@ -1,9 +1,6 @@
 class SiteController < ApplicationController
 	def home
-		
-		@concerts_today = Concert.where(date: Time.now..Time.now.end_of_day)
-    	@concerts_month = Concert.where(date: Time.now..Time.now.end_of_month)
-    	render 'home' #name of view template
+		 render 'home' #name of view template
     end
 
 end
