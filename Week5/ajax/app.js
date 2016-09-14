f59abf7a99e9ed35363c97d61a2197009461b2ff
Meneti Dissenha <menetidissenha@gmail.com>
2016-09-14 15:43:45 -0400

    Practice Ajax

--- Week5/ajax/app.js
+++ Week5/ajax/app.js
@@ -3,6 +3,9 @@ console.log("We are GO!");
 $(document).ready(function () {
 
 	$(".js-fetch-characters").on("click", fetchCharacters);
+	$(".js-add-kylo-ren").on("click", addKylo);
+	$(".js-add-new-character").on("click", addNewCharacter);
+
 
 
 function fetchCharacters () {
@@ -34,6 +37,52 @@ function showCharacters (response) {
 function handleError (error) {
 	console.log("Error!");
 	console.log(error.responseText);
+}
+
+function addKylo () {
+	var newCharacter = {
+		name: "Chewbacca",
+		occupation: "Muscle",
+		weapon: "Bowcaster"
+	};
+
+	$.ajax({
+	type: "POST",
+	url:"http://ironhack-characters.herokuapp.com/characters",
+	data: newCharacter,
+	success: updateList,
+	error:handleError
+	});
+	
+}
+
+function updateList (response) {
+	console.log("Success!");
+	console.log(response);
+};
+
+function addNewCharacter (theEvent) {
+		theEvent.preventDefault();
+
+		console.log("Add new Character click");
+
+		var newCharacterForm = {
+		weapon: $(".js-weapon").val(),
+		name: $(".js-name").val(),
+		occupation: $(".js-occupation").val()
+	};
+
+		$.ajax({
+	type: "POST",
+	url:"http://ironhack-characters.herokuapp.com/characters",
+	data: newCharacterForm,
+	success: updateList,
+	error:handleError
+	});
+
+
+
+		// console.log(`Weapon: ${weapon}`);
 }
 
 
